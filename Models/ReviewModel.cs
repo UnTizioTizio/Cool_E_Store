28c6836770ee48cd317f3f7818e658c532fd6393
UnTizioTizio <ludovico.iannello05@gmail.com>
2026-04-17 11:16:10 +0200

    Edit: new foreign key to User entity added in ReviewModel.cs

--- Models/ReviewModel.cs
+++ Models/ReviewModel.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CoolEStore.Models;
 
@@ -9,6 +10,9 @@ public class ReviewModel
     public required byte NStars { get; set; }
     public string? Title { get; set; }
     public string? Comment { get; set;}
+
+    public required int UserId { get; set; }
+    public required UserModel User { get; set; }
 
     public required int ProductId { get; set; }
     public required ProductModel Product { get; set; }
