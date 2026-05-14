9073368a041850f6c628cba09f55d94d593dc33c
UnTizioTizio <ludovico.iannello05@gmail.com>
2026-05-14 23:16:48 +0200

    Edit: replaced List<WarehouseRecord> WarehouseRecords property with List<ProductViewModel> Products in VendorViewModels

--- Models/VendorModel.cs
+++ Models/VendorModel.cs
@@ -15,37 +15,5 @@ public class VendorModel
     public ApplicationUserModel? ApplicationUser { get; set; }
 
     public List<WarehouseRecordModel>? WarehouseRecords { get; set; }
+    public List<ProductModel>? Products { get; set; }
 }
-
-// [Index(nameof(Email), nameof(PhoneNumber), IsUnique = true)]
-// public class VendorModel
-// {
-//     public int Id { get; set; }
-    
-//     [Required]
-//     [MaxLength(50), MinLength(3)]
-//     public required string Name { get; set; }
-    
-//     [Required]
-//     [EmailAddress]
-//     public required string Email { get; set; }
-    
-//     [Required]
-//     public required string Password { get; set; }
-
-//     [Required]
-//     [Phone]
-//     public required string PhoneNumber { get; set; }
-
-//     [Required]
-//     [StringLength(5)]
-//     public required string CAP { get; set; }
-    
-//     [Required]
-//     public required string Address { get; set; }
-    
-//     [Required]
-//     public ushort StreetNumber { get; set; }
-
-//     public List<WarehouseRecordModel>? WarehouseRecords { get; set; }
-// }
